0d7ea15d8325750323919be165b00349c77c8366
修名 <xiaocong.hxc@alibaba-inc.com>
2014-10-10 22:26:23 +0800

    去掉node_modules

--- rule.tmall.buy.js
+++ rule.tmall.buy.js
@@ -3,20 +3,13 @@ var proxy = require('./lib');
 
 module.exports = [
     {
-        pattern : 'http://g.assets.daily.taobao.net/??tm/buy-base/1.3.3/seed.js,tm/buy/1.1.1/seed.js,tm/buy/1.1.1/mio.js',
+        pattern : 'http://g.assets.daily.taobao.net/??tm/buy-base/1.3.3/seed.js,tm/buy/1.1.2/seed.js,tm/buy/1.1.2/mio.js',
         responder : proxy.mix(
-            proxy.route('kissy', {
-                packages: [{
-                    'name': 'mt',
-                    'path': '/gitlab/tb/buy/mt',
-                    'charset': 'gbk'
-                }],
-                input: '/gitlab/tb/buy/mt/index.js'
-            })
-/*
+            
             proxy.route('fetch', 'http://g.assets.daily.taobao.net/??tm/buy-base/1.3.3/seed.js'),
             proxy.route('local', '/gitlab/tm/buy/src/seed.js'),
-            proxy.route('concat', {
+            proxy.route('local', '/gitlab/tm/buy/src/mio.js')
+            /*proxy.route('concat', {
                 base : '/gitlab/tm/buy/src/',
                 files : [
                     'app',
@@ -45,10 +38,16 @@ module.exports = [
     },
 
 
-
+    {
+        pattern : 'http://g.tbcdn.cn/tm/buy/1.1.2/??',
+        responder : 'combo',
+        options : {
+          base: '/gitlab/tm/buy/src/'
+        }
+    },
 
     {
-        pattern : 'http://g.tbcdn.cn/tm/buy/1.1.1/',
+        pattern : 'http://g.tbcdn.cn/tm/buy/1.1.2/',
         responder : 'local',
         options : {
             file : '/gitlab/tm/buy/src/'
